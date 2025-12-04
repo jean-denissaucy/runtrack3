3ebf853ca520a62397e9ca398c91b1f18917a681
jean-denissaucy <jean-denis.saucy@laplateforme.io>
2025-12-04 14:33:08 +0100

    job1a6

---
+++ jour1/job4/script.js
@@ -0,0 +1,12 @@
+function bisexitille(annee) {
+    let result = (annee % 4 === 0 && annee % 100 != 0) || annee % 400 === 0
+        ? `${annee} est une année bissextile`
+        : `${annee} n'est pas une année bissextile`;
+    console.log(result);
+
+}
+bisexitille(2000);
+
+
+
+
---
+++ jour1/job5/script.js
@@ -0,0 +1,11 @@
+function afficherjoursemaines() {
+    const joursemaines = ["lundi, mardi, mercredi, jeudi, vendredi, samedi, dimanche"];
+
+    for (let i = 0; i < joursemaines.length; i++) {
+        console.log(joursemaines[i]);
+    }
+}
+
+afficherjoursemaines();
+
+
